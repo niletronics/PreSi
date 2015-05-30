c9ab8f3972fddf7eef8e9af7a7ad15a4da827610
Nilesh <niletronics@live.in>
2015-05-29 20:19:54 -0700

    Intermediate Release

---
+++ verify/Exe_Unit/exe_initiator.sv
@@ -0,0 +1,7 @@
+module exe_initiator  ( output logic base_addr,
+						output logic pdp_mem_opcode,
+						output logic pdp_op7_opcode,
+						input stall,
+						input PC_value);
+						
+						
--- verify/Exe_Unit/instr_exec_tb.sv
+++ verify/Exe_Unit/instr_exec_tb.sv
@@ -32,7 +32,7 @@ module instr_exec_tb();
    pdp_op7_opcode_s pdp_op7_opcode,  // Decoded signals for op7 instructions
 
    // To Initiator module
-   wire               stall,         // Signal to stall instruction decoder
+   wire               stall,         // Signal to stall Initiator
    wire [`ADDR_WIDTH-1:0] PC_value,      // Current value of Program Counter
 
    // To responder module
@@ -41,18 +41,21 @@ module instr_exec_tb();
    wire  [`DATA_WIDTH-1:0] exec_wr_data, // Write data to memory
    wire                    exec_rd_req,  // Read request to memory
    wire  [`ADDR_WIDTH-1:0] exec_rd_addr, // Read address
-
+   wire 				   ifu_rd_req,	  // Not to be used
+   wire  [`ADDR_WIDTH-1:0] ifu_rd_addr,   // Not to be used
+   wire  [`DATA_WIDTH-1:0] ifu_rd_data,   // Not to be used
+   
    // From responder module
    wire   [`DATA_WIDTH-1:0] exec_rd_data  // Read data returned by memory
 
-   //----------------ClockGen_Drive Instantiation-------------//
+   //---------------------ClockGen_Drive Instantiation-------------//
       clkgen_driver #(
       .CLOCK_PERIOD(10),
       .RESET_DURATION(500)) clkgen_exe (
       .clk     (clk),
       .reset_n (reset_n));
-	//-------------------------DUT Instantiation-------------//
-	instr_exec DUT(
+	//---------------------DUT Instantiation-----------------------//
+	instr_exec	DUT(
    // From clkgen_driver module
    .clk,                              // Free running clock
    .reset_n,                          // Active low reset signal
@@ -76,30 +79,51 @@ module instr_exec_tb();
    // From responder module
    .exec_rd_data  // Read data returned by responder
    );
-   
-   //------------------------------Memory Instantiation-------------//
-   
-   memory_pdp      exe_memory_pdp(
+    //------------------Responder/Memory Instantiation-------------//
+    memory_pdp	exe_memory_pdp(
    //------From ClockGen_Driver------------
 	.clk,
-	
+	//--------------------Not to be driven------------------//
 	.ifu_rd_req,
 	.ifu_rd_addr,
 	.ifu_rd_data,
-	
+	//---------------To be used as memory--------------------//
+	//read signals
 	.exec_rd_req,
 	.exec_rd_addr,
 	.exec_rd_data,
-	
+	//write signals
 	.exec_wr_req,
 	.exec_wr_addr,
 	.exec_wr_data   
    );
-
-   
-   
-   
-   
+	//-------------------Initiator Instantiation-------------------//
+	exe_initiator  initiator_exe(
+	.base_addr,
+	.pdp_mem_opcode,
+	.pdp_op7_opcode,
+	.stall,
+	PC_value
+	);
+   //---------------------Checker Instantiation--------------------//
+	checker_exe ckr(
+	.clk,
+	.reset_n,
+	.base_addr,
+	.pdp_mem_opcode,
+	.pdp_op7_opcode,
+	.stall,
+	.PC_value,
+	.exec_wr_req,
+	.exec_wr_addr,
+	.exec_wr_data,
+	.exec_rd_req, 
+	.exec_rd_addr,
+	.ifu_rd_req,	
+    .ifu_rd_addr, 
+    .ifu_rd_data,
+	.exec_rd_data
+	);
 
 
 	  
