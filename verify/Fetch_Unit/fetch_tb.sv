8804d117d46ce2dc3b725872f698195155faa398
nverma-pdx <neetiverma09@gmail.com>
2015-06-04 16:49:52 -0700

    made testbench

--- verify/Fetch_Unit/fetch_tb.sv
+++ verify/Fetch_Unit/fetch_tb.sv
@@ -14,3 +14,25 @@
 //   Created by:   Neeti Verma
 //   Date:         May 25, 2015
 // =======================================================================
+
+`include "pdp8_pkg.sv"
+import pdp8_pkg::*;
+
+module testbench;
+  wire clk;
+  wire reset_n, stall, ifu_rd_req;
+  wire [`ADDR_WIDTH-1:0] PC_value;             
+  wire  [`ADDR_WIDTH-1:0] ifu_rd_addr;
+  wire [`DATA_WIDTH-1:0] ifu_rd_data;
+  wire [`ADDR_WIDTH-1:0] base_addr = `START_ADDRESS;
+  pdp_mem_opcode_s pdp_mem_opcode;
+  pdp_op7_opcode_s pdp_op7_opcode;
+  
+  
+  clkgen_driver clk_module ( .clk, .reset_n);
+  fetch_stimuli_gen SG ( .ifu_rd_req, .num_stimuli, .stall, .generated_data, .PC_value);
+  fetch_chkr CHKR (.clk, .reset_n, .stall, .PC_value, .ifu_rd_req, .ifu_rd_addr, .ifu_rd_data, .base_addr, .pdp_mem_opcode, .pdp_op7_opcode);
+  instr_decode DUT (.clk, .stall, .PC_value, .ifu_rd_req, .ifu_rd_addr, .ifu_rd_data, .base_addr, .pdp_mem_opcode, .pdp_op7_opcode);
+   );
+
+endmodule
--- verify/Fetch_Unit/filelist.f
+++ verify/Fetch_Unit/filelist.f
@@ -1,2 +1,4 @@
 ../../RTL/hdl/pdp8_pkg.sv
 fetch_chkr.sv
+fetch_stimuli_gen.sv
+fetch_tb.sv
